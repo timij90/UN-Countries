ca28878de713c83b0e86e52cbff2669837e96037
Timi Jacobs Uba <oujacobs@myseneca.ca>
2024-02-01 13:14:45 -0500

    assignment 2

---
+++ modules/unCountries.js
@@ -0,0 +1,61 @@
+const countryData = require("../data/countryData.json");
+const regionData = require("../data/regionData.json");
+
+let countries = [];
+
+function Initialize() {
+    return new Promise((resolve, reject) => {
+        // Iterate through each countryData object
+        countryData.forEach(country => {
+            // Find the corresponding region object
+            const region = regionData.find(region => region.id === country.regionId);
+
+            // Create a new country object with the additional "region" property
+            const newCountry = {
+                ...country,
+                region
+            };
+
+            // Push the new country object into the countries array
+            countries.push(newCountry);
+        });
+        resolve();
+    });
+}
+
+function getAllCountries() {
+    return new Promise((resolve, reject) => {
+        resolve(countries);
+    });
+}
+
+function getCountryByCode(countryCode) {
+    return new Promise((resolve, reject) => {
+        const country = countries.find(country => country.a2code.toLowerCase() === countryCode.toLowerCase());
+
+        if (country) {
+            resolve(country);
+        } else {
+            reject(new Error("unable to find requested country"));
+        }
+    });
+}
+
+function getCountriesByRegion(region) {
+    return new Promise((resolve, reject) => {
+        const filteredCountries = countries.filter(country => country.region.name.toLowerCase().includes(region.toLowerCase()));
+
+        if (filteredCountries.length > 0) {
+            resolve(filteredCountries);
+        } else {
+            reject(new Error("unable to find requested countries"));
+        }
+    });
+}
+
+module.exports = {
+    Initialize,
+    getAllCountries,
+    getCountryByCode,
+    getCountriesByRegion
+};
---
+++ server.js
@@ -0,0 +1,49 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Jacobs Oluwatimilehin Uba Student ID: 148981228 Date: 01/02/2024
+*
+*  Online (Cyclic) Link: 
+*
+********************************************************************************/
+
+
+
+const express = require('express'); // "require" the Express module
+const app = express(); // obtain the "app" object
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
+
+// start the server on the port and output a confirmation to the console
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+
+const unCountryData = require("./modules/unCountries");
+
+app.get('/', (req, res) => {
+  res.send("Assignment 2:  Jacobs Oluwatimilehin Uba - 148981228");
+});
+
+unCountryData.Initialize()
+  .then(() => {
+
+    app.get('/un/countries', (req, res) => {
+      res.json(unCountryData.getAllCountries());
+    });
+  })
+  .catch(error => console.error('Error initializing data:', error));
+
+app.get('/un/countries/code-demo', (req, res) => {
+  const countryCode = 'ca'; // Replace with the desired country code
+  unCountryData.getCountryByCode(countryCode)
+    .then(country => res.json(country))
+    .catch(error => res.status(500).send('Error: ' + error.message));
+});
+
+app.get('/un/countries/region-demo', (req, res) => {
+  const region = 'oceania'; // Replace with the desired region
+  unCountryData.getCountriesByRegion(region)
+    .then(countries => res.json(countries))
+    .catch(error => res.status(500).send('Error: ' + error.message));
+});
